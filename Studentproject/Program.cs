4f295ddb7bdd8d7306c1f7e2ad8c6eb91a2636d5
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-14 10:01:16 +0530

    Add project files.

---
+++ Studentproject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.AssignValue();
+            string student = student.DisplayValue();
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ Studentproject/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Studentproject
+{
+    internal class Student
+    {
+        int id, age;
+        string name, address;
+        public void AssignValue()
+        {
+            id = 11;
+            name = "Shubhangi";
+            age = 21;
+            address = "D.Awari";
+        }
+        public string DisplayValue()
+        {
+            return $"Student detail:{id}/{name}/{age}/{address}";
+        }
+
+
+    }
+}
